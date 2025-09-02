b93d034e9deaa012358f488cfad46f3e689b69f4
RANU GUPTA <164221752+Ranugpt@users.noreply.github.com>
2025-09-02 20:28:59 +0530

    Fixed components imports and added missing files

--- src/assets/recipe project by react/RECIPES BACKEND/DB/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://guptaranu719:WAPo9cQHVeweIGPV@cluster0.yddz4tc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
-    // useNewUrlParser: true,
-    // useUnifiedTopology:true
-}).then(()=>{
-    console.log("Database Connected");
-}).catch((err)=>{
-    console.log(err);
-})
--- src/assets/recipe project by react/RECIPES BACKEND/routes/userRout.js
+++
@@ -1,8 +0,0 @@
-const userController = require('../Controller/userController');
-const express = require('express');
-const {application} = require('express');
-
-const router = express.Router();
-
-router.post('/saveuser', userController.saveUser);
-module.exports = router;
