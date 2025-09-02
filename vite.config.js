935c99f83fbcd3ddc69b4264fa50f76b9c88defb
RANU GUPTA <164221752+Ranugpt@users.noreply.github.com>
2025-09-02 21:10:14 +0530

    error fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(),tailwindcss()],
+  // [tailwindcss()],
+  base: "./",   // 👈 this makes assets relative instead of absolute
+});
