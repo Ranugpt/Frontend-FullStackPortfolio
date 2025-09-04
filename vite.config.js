b96ffab749d3826dd0c4065facf196ae36b10665
RANU GUPTA <164221752+Ranugpt@users.noreply.github.com>
2025-09-04 18:57:07 +0530

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// })
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  // [tailwindcss()],
   base: "./",   // 👈 this makes assets relative instead of absolute
 });
